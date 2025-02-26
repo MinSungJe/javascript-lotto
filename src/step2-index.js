fb5c76b88dc2bf0208e5b00e2292b56e1d608142
Minje Sung <minje813@dgu.ac.kr>
2025-02-26 15:11:38 +0900

    feat: 당첨 통계 모달창 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,8 +2,13 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+const app = document.querySelector("#app");
 const lottoContainer = document.querySelector(".lotto-container");
-const lottoTicketTemplate = document.getElementById("lotto-ticket");
 
+const resultModalTemplate = document.querySelector("#result-modal");
+const lottoTicketTemplate = document.querySelector("#lotto-ticket");
+
+const resultModalClone = resultModalTemplate.content.cloneNode(true);
 const lottoTicketClone = lottoTicketTemplate.content.cloneNode(true);
+app.prepend(resultModalClone);
 lottoContainer.appendChild(lottoTicketClone);
