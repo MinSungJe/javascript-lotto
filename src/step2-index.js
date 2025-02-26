b615f4dd653d1b43c7ed9d5abb595b17a248cef0
Minje Sung <minje813@dgu.ac.kr>
2025-02-26 14:36:19 +0900

    feat: Lotto Template 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,8 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+const lottoContainer = document.querySelector(".lotto-container");
+const lottoTicketTemplate = document.getElementById("lotto-ticket");
+
+const lottoTicketClone = lottoTicketTemplate.content.cloneNode(true);
+lottoContainer.appendChild(lottoTicketClone);
